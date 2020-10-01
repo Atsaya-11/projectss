46c5a3c0e91b64cefe4fd078c7fb937a5d5ab5b4
cloud-storage-571 <72211563+cloud-storage-571@users.noreply.github.com>
2020-10-01 20:04:05 +0530

    Update WarshallActivity.java

--- app/src/main/java/com/src/graphapp/activities/WarshallActivity.java
+++ app/src/main/java/com/src/graphapp/activities/WarshallActivity.java
@@ -115,49 +115,49 @@ public class WarshallActivity extends AppCompatActivity implements View.OnClickL
         tvFW[5][8] = (TextView)findViewById(R.id.tv58);
         tvFW[5][9] = (TextView)findViewById(R.id.tv59);
 
-        tvFW[6][0] = (TextView)findViewById(R.id.tv60);
-        tvFW[6][1] = (TextView)findViewById(R.id.tv61);
-        tvFW[6][2] = (TextView)findViewById(R.id.tv62);
-        tvFW[6][3] = (TextView)findViewById(R.id.tv63);
-        tvFW[6][4] = (TextView)findViewById(R.id.tv64);
-        tvFW[6][5] = (TextView)findViewById(R.id.tv65);
-        tvFW[6][6] = (TextView)findViewById(R.id.tv66);
-        tvFW[6][7] = (TextView)findViewById(R.id.tv67);
-        tvFW[6][8] = (TextView)findViewById(R.id.tv68);
-        tvFW[6][9] = (TextView)findViewById(R.id.tv69);
+        tvFW[6][0] = findViewById(R.id.tv60);
+        tvFW[6][1] = findViewById(R.id.tv61);
+        tvFW[6][2] = findViewById(R.id.tv62);
+        tvFW[6][3] = findViewById(R.id.tv63);
+        tvFW[6][4] = findViewById(R.id.tv64);
+        tvFW[6][5] = findViewById(R.id.tv65);
+        tvFW[6][6] = findViewById(R.id.tv66);
+        tvFW[6][7] = findViewById(R.id.tv67);
+        tvFW[6][8] = findViewById(R.id.tv68);
+        tvFW[6][9] = findViewById(R.id.tv69);
 
-        tvFW[7][0] = (TextView)findViewById(R.id.tv70);
-        tvFW[7][1] = (TextView)findViewById(R.id.tv71);
-        tvFW[7][2] = (TextView)findViewById(R.id.tv72);
-        tvFW[7][3] = (TextView)findViewById(R.id.tv73);
-        tvFW[7][4] = (TextView)findViewById(R.id.tv74);
-        tvFW[7][5] = (TextView)findViewById(R.id.tv75);
-        tvFW[7][6] = (TextView)findViewById(R.id.tv76);
-        tvFW[7][7] = (TextView)findViewById(R.id.tv77);
-        tvFW[7][8] = (TextView)findViewById(R.id.tv78);
-        tvFW[7][9] = (TextView)findViewById(R.id.tv79);
+        tvFW[7][0] = findViewById(R.id.tv70);
+        tvFW[7][1] = findViewById(R.id.tv71);
+        tvFW[7][2] = findViewById(R.id.tv72);
+        tvFW[7][3] = findViewById(R.id.tv73);
+        tvFW[7][4] = findViewById(R.id.tv74);
+        tvFW[7][5] = findViewById(R.id.tv75);
+        tvFW[7][6] = findViewById(R.id.tv76);
+        tvFW[7][7] = findViewById(R.id.tv77);
+        tvFW[7][8] = findViewById(R.id.tv78);
+        tvFW[7][9] = findViewById(R.id.tv79);
 
-        tvFW[8][0] = (TextView)findViewById(R.id.tv80);
-        tvFW[8][1] = (TextView)findViewById(R.id.tv81);
-        tvFW[8][2] = (TextView)findViewById(R.id.tv82);
-        tvFW[8][3] = (TextView)findViewById(R.id.tv83);
-        tvFW[8][4] = (TextView)findViewById(R.id.tv84);
-        tvFW[8][5] = (TextView)findViewById(R.id.tv85);
-        tvFW[8][6] = (TextView)findViewById(R.id.tv86);
-        tvFW[8][7] = (TextView)findViewById(R.id.tv87);
-        tvFW[8][8] = (TextView)findViewById(R.id.tv88);
-        tvFW[8][9] = (TextView)findViewById(R.id.tv89);
+        tvFW[8][0] = findViewById(R.id.tv80);
+        tvFW[8][1] = findViewById(R.id.tv81);
+        tvFW[8][2] = findViewById(R.id.tv82);
+        tvFW[8][3] = findViewById(R.id.tv83);
+        tvFW[8][4] = findViewById(R.id.tv84);
+        tvFW[8][5] = findViewById(R.id.tv85);
+        tvFW[8][6] = findViewById(R.id.tv86);
+        tvFW[8][7] = findViewById(R.id.tv87);
+        tvFW[8][8] = findViewById(R.id.tv88);
+        tvFW[8][9] = findViewById(R.id.tv89);
 
-        tvFW[9][0] = (TextView)findViewById(R.id.tv90);
-        tvFW[9][1] = (TextView)findViewById(R.id.tv91);
-        tvFW[9][2] = (TextView)findViewById(R.id.tv92);
-        tvFW[9][3] = (TextView)findViewById(R.id.tv93);
-        tvFW[9][4] = (TextView)findViewById(R.id.tv94);
-        tvFW[9][5] = (TextView)findViewById(R.id.tv95);
-        tvFW[9][6] = (TextView)findViewById(R.id.tv96);
-        tvFW[9][7] = (TextView)findViewById(R.id.tv97);
-        tvFW[9][8] = (TextView)findViewById(R.id.tv98);
-        tvFW[9][9] = (TextView)findViewById(R.id.tv99);
+        tvFW[9][0] = findViewById(R.id.tv90);
+        tvFW[9][1] = findViewById(R.id.tv91);
+        tvFW[9][2] = findViewById(R.id.tv92);
+        tvFW[9][3] = findViewById(R.id.tv93);
+        tvFW[9][4] = findViewById(R.id.tv94);
+        tvFW[9][5] = findViewById(R.id.tv95);
+        tvFW[9][6] = findViewById(R.id.tv96);
+        tvFW[9][7] = findViewById(R.id.tv97);
+        tvFW[9][8] = findViewById(R.id.tv98);
+        tvFW[9][9] = findViewById(R.id.tv99);
     }
 
     public void displayFWMatrix(){
@@ -188,8 +188,8 @@ public class WarshallActivity extends AppCompatActivity implements View.OnClickL
         description = i1.getStringExtra("description");
         complexity = i1.getStringExtra("complexity");
 
-        bDescription = (Button)findViewById(R.id.bDescription);
-        bHelp = (Button) findViewById(R.id.bHelp);
+        bDescription = findViewById(R.id.bDescription);
+        bHelp = findViewById(R.id.bHelp);
 
         bHelp.setOnClickListener(this);
         bDescription.setOnClickListener(this);
